29bbd925a07253774763301f030468b497030b1e
MultiMind <multimind@multimind>
2020-07-01 19:42:15 -0400

    Agrega lectura de img, video, carpeta/imgs, carpeta/videos

--- detect.py
+++ detect.py
@@ -52,16 +52,11 @@ def formato_hora(hora):
     seg = hora[4:6]
     return str(hrs+':'+min+':'+seg)
 
-def input_video(model,path,namesfile,carpeta_save,use_cuda):
+def input_video(model,path,namesfile,carpeta_save,use_cuda,mode=None):
     cap = cv2.VideoCapture(path)
     fps    = cap.get(cv2.CAP_PROP_FPS)
     print('fps',fps)
 
-    fecha = path.split("_")[1].split(".")[0][0:8]
-    fecha_formato_reporte = str(fecha[6:8]+'.'+fecha[4:6]+'.'+fecha[0:4])
-    hora_ruta = path.split("_")[1].split(".")[0][8:14]
-    hora_inicio = formato_hora(hora_ruta)
-    print('fecha: ',fecha,' fecha formato', fecha_formato_reporte, 'hora_ruta',hora_ruta, 'hora inicio',hora_inicio)
 
     frameCount = 0
     tren = []
@@ -98,27 +93,19 @@ def input_video(model,path,namesfile,carpeta_save,use_cuda):
             tren.append(0)
             data.loc[frameCount] = [frameCount,"NO",str(time.ctime())] 
         frameCount +=1
-        
-        """
-        if(sum(tren[len(tren)-40:len(tren)])>=1):
-            hora_detect = milisegundos_a_hora(cap.get(cv2.CAP_PROP_POS_MSEC))
-            
-          #  print("Deteccion de camion, frame ",frameCount,' hora detect',hora_detect[0:6],'hora grab',sumar_hora(hora_detect,hora_inicio) )
-            hora.append(sumar_hora(hora_inicio,hora_detect[0:6]))
-            frames.append(frameCount)
-        if (sum(tren[len(tren)-40: len(tren)]) == 0 and sum(tren[len(tren)-80: len(tren)-40]) >= 1 ):
-        #    print("se fue el camion",frameCount)
-            hora_detect2 = milisegundos_a_hora(cap.get(cv2.CAP_PROP_POS_MSEC))
-            data.loc[len(data)]=[hora[0],frames[0],sumar_hora(hora_inicio,hora_detect2[0:6]),frameCount] 
-        """    
+    if(mode == "dvideo"):
+        print("Modo dvideo")   
+        #print(path.split(".")[0].split("/")[1])
+        name_xlsx = path.split(".")[0].split("/")[1]
+        data.to_excel("./registro_frame/"+str(name_xlsx)+'.xlsx', sheet_name='detectados' , index=False)
+    else:
+        print(path.split(".")[0])
+        name_xlsx = path.split(".")[0]
+        data.to_excel("./registro_frame/"+str(name_xlsx)+'.xlsx', sheet_name='detectados' , index=False)
 
-            #del tren[:], hora[:], frames[:]
-    print(path.split("/")[7].split(".")[0])
-    name_xlsx = path.split("/")[7].split(".")[0]
-    data.to_excel("./registro_frame/"+str(name_xlsx)+'.xlsx', sheet_name='detectados' , index=False)
     print(fps)
     print('Fotogramas Procesados',frameCount)
-    return fps
+    return fps 
 
 def input_dir_image(model, path, namesfile,use_cuda):
     imagenes = os.listdir(path)
@@ -140,7 +127,7 @@ def input_dir_image(model, path, namesfile,use_cuda):
         class_names = load_class_names(namesfile)
         plot_boxes(img, boxes, folder_predictions+'/'+str(imagenes[z]).split(".")[0]+'-predictions.jpg', class_names)
         tiempo_de_prediccion.append((finish - start))
-    print('Tiempo de Procesamiento:',sum(tiempo_de_prediccion))
+    print('Tiempo de Procesamiento:',sum(tiempo_de_prediccion),"seg")
 
 def input_image(model,path,namesfile,use_cuda):
     img = Image.open(path).convert('RGB')
@@ -156,7 +143,7 @@ def input_image(model,path,namesfile,use_cuda):
     plot_boxes(img, boxes, 'predictions.jpg', class_names)
 
 
-def detect(cfgfile, weightfile, path, formato,carpeta_save=None):
+def detect(cfgfile, weightfile, path, formato,carpeta_save=None,mode=None):
     inicio = time.time()
     m = Darknet(cfgfile)
 
@@ -181,106 +168,55 @@ def detect(cfgfile, weightfile, path, formato,carpeta_save=None):
     if(formato == 1):
         input_dir_image(m,path,namesfile,use_cuda)
     if(formato == 2):
-        fps = input_video(m,path,namesfile,carpeta_save,use_cuda)
+        fps = input_video(m,path,namesfile,carpeta_save,use_cuda,mode)
         convert_img_video(carpeta_save,fps)
     print('T',time.time()-inicio)
 
-    def flujo(ruta):
-        while(1):
-            inicio = time.time()
-            t_end = inicio + 10 * 1
-            estado.append(Path(os.path.abspath(path+"/"+ruta)).stat().st_size)
-            while(time.time() < t_end):
-                print(Path(os.path.abspath(path+"/"+ruta)).stat().st_size)
-            estado.append(Path(os.path.abspath(path+"/"+ruta)).stat().st_size)
-            print(str(estado[len(estado)-2])+"  "+str(estado[len(estado)-1]))
-            if(estado[len(estado)-2] == estado[len(estado)-1] ):
-                    print("no hubo modificacion")
-                    break
-        return True
+def flujo(ruta):
+    while(1):
+        inicio = time.time()
+        t_end = inicio + 10 * 1
+        estado.append(Path(os.path.abspath(path+"/"+ruta)).stat().st_size)
+        while(time.time() < t_end):
+            print(Path(os.path.abspath(path+"/"+ruta)).stat().st_size)
+        estado.append(Path(os.path.abspath(path+"/"+ruta)).stat().st_size)
+        print(str(estado[len(estado)-2])+"  "+str(estado[len(estado)-1]))
+        if(estado[len(estado)-2] == estado[len(estado)-1] ):
+            print("no hubo modificacion")
+            break
+    return True
 
 if __name__ == '__main__':
-    cfgfile = "cfg/yolo-camion-darknet.cfg"
-    weightfile = "backup/yolo-camion_14000.weights"
-    carpeta_videos = "./video-predict"
-
-    path = "./video-cam"
-
-    estado = []
-
-
-
-    #print(os.listdir(path)[0])
-    #flujo(os.listdir(path)[0])
-
-    while(1):
-        if(len(os.listdir(path))>0):
-            fl = flujo(os.listdir(path)[0])
-            if fl:
-                print("llamar detector")
-                imgfile = os.path.abspath(path+"/"+os.listdir(path)[0])
-                print(imgfile.split("/")[7].split(".")[0])
-                carpeta_save = carpeta_videos+"/"+str(imgfile).split(".")[0].split("/")[7]
-                if(os.path.isdir(carpeta_videos) == False):
-                    os.mkdir(carpeta_videos)
-                if(os.path.isdir(carpeta_save) == False):
-                    os.mkdir(carpeta_save)
-                detect(cfgfile, weightfile, imgfile, 2,carpeta_save)
-                shutil.move(imgfile,os.path.abspath("videos-ok"+"/"+os.listdir(path)[0]))
-                #break
-
-
-
-
-    """    
-    if len(sys.argv) == 2:
-        star = time.time()
-        #cfgfile = sys.argv[1]
-        #weightfile = sys.argv[2]
-        #imgfile = sys.argv[3]
-        print(sys.argv[1])
-        cfgfile = "cfg/yolo-camion-darknet.cfg"
-        weightfile = "backup/yolo-camion_14000.weights"
-        #imgfile = "./video-desarrollo/ch06_20200106101038.mp4" # Directorio de videos
-        imgfile = sys.argv[1]
-        #imgfile = "./946.jpg" # una imagen
-        #imgfile = "./img-desarrollo" # directorio de imagenes
+    if len(sys.argv) == 5:
+        cfgfile = sys.argv[1]
+        weightfile = sys.argv[2]
+        option = sys.argv[3]
+        imgfile = sys.argv[4]
         carpeta_videos = "./video-predict"
-        
-        
-        if(os.path.isfile(imgfile)): # Una imagen
-            "Una Imagen"
-            if(imgfile.split(".")[2] == "jpg"):
-                detect(cfgfile, weightfile, imgfile, 0)
-            "Un Video"
-            if(imgfile.split(".")[2] == "mp4"):
-                carpeta_save = carpeta_videos+"/"+str(imgfile).split(".")[1].split("/")[2]
-                if(os.path.isdir(carpeta_videos) == False):
-                    os.mkdir(carpeta_videos)
-                if(os.path.isdir(carpeta_save) == False):
-                    os.mkdir(carpeta_save)
-                detect(cfgfile, weightfile, imgfile, 2,carpeta_save)
-        "Directorios"
-        if(os.path.isdir(imgfile)):
-            "Directorio de Imagenes"
-            if(os.listdir(imgfile)[0].split(".")[1] == "jpg"):
-                detect(cfgfile, weightfile, imgfile, 1)
-            "Directorio de Videos"
-            if(os.listdir(imgfile)[0].split(".")[1] == "mp4"):
-                videos = os.listdir(imgfile)
-                for i in range(len(videos)):
-                    carpeta_save = carpeta_videos+"/"+str(videos[i]).split(".")[0]
-                    path = imgfile+"/"+str(videos[i])
-                    if(os.path.isdir(carpeta_videos) == False):
-                        os.mkdir(carpeta_videos)
-                    if(os.path.isdir(carpeta_save) == False):
-                        os.mkdir(carpeta_videos+"/"+str(videos[i]).split(".")[0])
-                    detect(cfgfile, weightfile, path, 2, carpeta_save)
-                    convert_img_video(carpeta_save)
-        #
-        
-    else:
-        print('Usage: ')
-        print('  python detect.py cfgfile weightfile imgfile')
-        # detect('cfg/tiny-yolo-voc.cfg', 'tiny-yolo-voc.weights', 'data/person.jpg', version=1)
-    """
+
+        if(option == "video"):
+            print("option video")
+            carpeta_save = carpeta_videos+"/"+str(imgfile).split(".")[0]
+            print(carpeta_save)
+            if(os.path.isdir(carpeta_save) == False):
+                os.mkdir(carpeta_save)
+            detect(cfgfile, weightfile, imgfile, 2,carpeta_save)
+        if(option == "img"):
+            print("option imagen")
+            detect(cfgfile, weightfile, imgfile, 0)
+        if(option == "dimg"):
+            print("directorio de imagen")
+            detect(cfgfile, weightfile, imgfile, 1)
+        if(option == "dvideo"):
+            print("directorio de videos")
+            videos = os.listdir(imgfile)
+            print(videos)
+            for vd in videos:
+                print(vd.split(".")[0])
+                carpeta_vd = vd.split(".")[0]
+                carpeta_save = carpeta_videos+"/"+vd.split(".")[0]
+                path = imgfile+"/"+str(vd)
+                print(path)
+                if(os.path.isdir(carpeta_videos+"/"+str(carpeta_vd)) == False):
+                    os.mkdir(carpeta_videos+"/"+str(carpeta_vd))
+                detect(cfgfile, weightfile, path, 2, carpeta_save,"dvideo")
